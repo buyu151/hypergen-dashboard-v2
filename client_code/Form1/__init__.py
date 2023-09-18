4444f49873f95d483539feaed68b10cd307106de
nicolas.schulz@proscientia.co.uk <nicolas.schulz@proscientia.co.uk>
2023-09-18 16:59:18 +0000

    Created ServerModule1 and HyperGen-Logo-Icon-POS.png; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
     def __init__(self, **properties):
@@ -7,3 +8,47 @@ class Form1(Form1Template):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
+
+    def power_dd_change(self, **event_args):
+        """This method is called when an item is selected"""
+        pass
+
+    def runt_time_dd_change(self, **event_args):
+        """This method is called when an item is selected"""
+        pass
+
+    def op_days_dd_change(self, **event_args):
+        """This method is called when an item is selected"""
+        pass
+
+    def solar_irr_dd_change(self, **event_args):
+        """This method is called when an item is selected"""
+        pass
+
+    def drop_down_3_change(self, **event_args):
+        """This method is called when an item is selected"""
+        pass
+
+    def drop_down_7_change(self, **event_args):
+        """This method is called when an item is selected"""
+        pass
+
+    def drop_down_4_change(self, **event_args):
+        """This method is called when an item is selected"""
+        pass
+
+    def drop_down_8_change(self, **event_args):
+        """This method is called when an item is selected"""
+        pass
+
+
+
+
+
+
+
+
+
+    
+
+
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,4 @@
+import anvil.server
+
+
+
