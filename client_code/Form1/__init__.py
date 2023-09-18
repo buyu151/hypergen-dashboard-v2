52da8ac508eba3170bc1ea14222e952e2d6abf3f
nicolas.schulz@proscientia.co.uk <nicolas.schulz@proscientia.co.uk>
2023-09-18 18:08:51 +0000

    Edited settings, ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,13 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.server
+import plotly.graph_objects as go
+import time
+from datetime import datetime
+# from random import randint
 
 class Form1(Form1Template):
     def __init__(self, **properties):
@@ -9,37 +16,248 @@ class Form1(Form1Template):
 
         # Any code you write here will run before the form opens.
 
-    def power_dd_change(self, **event_args):
+     #-----------------------------------------------------------------------------------------------------------
+        #Variables drop down menus:
+
+        t_begin = time.time()
+        
+        self.avg_power = ['20', '30', '40', '60', '75', '100', '125', '135', '150', '175', '200']
+        self.dd_avg_power.items = self.avg_power #dd for drop down menue
+        self.dd_avg_power.selected_value = '30' #Default value in drop down menue
+        # print(f'Average power\n{self.avg_power}\n')
+        
+        self.run_time = [ str(item) for item in range(1, 24+1) ]
+        self.dd_run_time.items = self.run_time
+        self.dd_run_time.selected_value = '8' #Default value
+        # print(f'Run time\n{self.run_time}\n')
+        
+        self.days_year = [ str(item) for item in range(1, 365+1)]
+        self.dd_days_year.items = self.days_year
+        self.dd_days_year.selected_value = '250' #Default value
+        # print(f'Days of the year operating\n{self.days_year}\n')
+        
+        
+        self.solar_irrad = ['1.5', '2.6', '3', '4', '5.45', '6']
+        self.dd_solar_irrad.items = self.solar_irrad
+        self.dd_solar_irrad.selected_value = '5.45' #Default value
+        # Solar radiation can be categorized into four classes: levels less than 2.6 kWh/m2 
+        # are classified as low solar radiation while solar irradiance between 2.6-3 kWh/m2 
+        # is moderate solar radiation; irradiance of between 3-4 kWh/m2 is high solar radiation
+        # and irradiance higher than 4 kWh/m2 is very high radiation.
+        # print(f'Average solar irradiation\n{self.solar_irrad}\n')
+        
+        self.wind_speed = [str(item) for item in range(2, 15+1)]
+        self.dd_wind_speed.items = self.wind_speed
+        self.dd_wind_speed.selected_value = '5' #Default value
+        # print(f'Average wind speed\n{self.wind_speed}\n')
+        
+        self.fuel_cost = [str(1 + (item/10)) for item in range(0, 11) ]
+        self.dd_fuel_cost.items = self.fuel_cost
+        self.dd_fuel_cost.selected_value = '1.0' #Default value
+        # print(f'Average fuel cost\n{self.fuel_cost}\n')
+        
+        self.elect_grid_cost = [str(round((3 + (item/10))/10, 2)) for item in range(4, 70)]
+        self.dd_elect_grid_cost.items = self.elect_grid_cost
+        self.dd_elect_grid_cost.selected_value = '0.34' #Default value
+        # print(f'Electric grid cost\n{self.elect_grid_cost}\n')
+
+        self.energy_inflation = [ str(item) for item in range(10, 101, 10)]
+        self.dd_energy_inflation.items = self.energy_inflation
+        self.dd_energy_inflation.selected_value = '30' #Default value
+        # print(f'Energy inflation\n{self.energy_inflation}\n')
+
+        t_end = time.time()
+        print(f'Build drop down list values in {t_end-t_begin} seconds')
+        
+       
+        #-----------------------------------------------------------------------------------------------------------
+        #Delete previous run
+        # anvil.server.call('delete_cumulative_costs')
+        
+
+
+    def button_run_click(self, **event_args):
+        
+        """This method is called when the button is clicked"""
+
+        #-----------------------------------------------------------------------------------------------------------
+        #Get user id
+        self.user_id = anvil.server.call('get_uuid')
+        print(f'User id is {self.user_id}')
+        
+        #-----------------------------------------------------------------------------------------------------------
+        #Upload inputs to server:
+
+        self.session_time = datetime.now()
+
+        t_begin_total = time.time()
+
+        t_begin = time.time()
+
+        anvil.server.call('get_inputs',
+                          self.dd_avg_power.selected_value,
+                          self.dd_run_time.selected_value,
+                          self.dd_days_year.selected_value,
+                          self.dd_solar_irrad.selected_value,
+                          self.dd_wind_speed.selected_value,
+                          self.dd_elect_grid_cost.selected_value,
+                          self.dd_energy_inflation.selected_value,
+                          self.dd_fuel_cost.selected_value,
+                          self.user_id
+                         )
+
+
+        t_end = time.time()
+        print(f'Done uploading inputs to server in {t_end-t_begin} seconds')
+        #-----------------------------------------------------------------------------------------------------------
+        #Runcalculations server:
+        t_begin = time.time()
+        
+        inputs ={}
+
+        inputs["avg_pwr"] = self.dd_avg_power.selected_value
+        inputs["run_time"] = self.dd_run_time.selected_value
+        inputs["days_op_per_year"] = self.dd_days_year.selected_value
+        inputs["avg_solar_irr"] = self.dd_solar_irrad.selected_value
+        inputs["avg_wind_speed"] = self.dd_wind_speed.selected_value
+        inputs["cost_electric"] = self.dd_elect_grid_cost.selected_value
+        inputs["energy_inflation"] = self.dd_energy_inflation.selected_value
+        inputs["cost_fuel"] = self.dd_fuel_cost.selected_value
+
+        anvil.server.call('run_calcs', inputs)
+
+        t_end = time.time()
+        print(f'Done doing calculations in server in {t_end-t_begin} seconds')
+
+        t_end_total = time.time()
+
+        print(f"Total run time {t_end_total-t_begin_total} seconds")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    def dd_avg_power_change(self, **event_args):
         """This method is called when an item is selected"""
         pass
 
-    def runt_time_dd_change(self, **event_args):
+    def dd_days_year_change(self, **event_args):
         """This method is called when an item is selected"""
         pass
 
-    def op_days_dd_change(self, **event_args):
+    def dd_run_time_change(self, **event_args):
         """This method is called when an item is selected"""
         pass
 
-    def solar_irr_dd_change(self, **event_args):
+    def dd_solar_irrad_change(self, **event_args):
         """This method is called when an item is selected"""
         pass
 
-    def drop_down_3_change(self, **event_args):
+    def dd_wind_speed_change(self, **event_args):
         """This method is called when an item is selected"""
         pass
 
-    def drop_down_7_change(self, **event_args):
+    def dd_elect_grid_cost_change(self, **event_args):
         """This method is called when an item is selected"""
         pass
 
-    def drop_down_4_change(self, **event_args):
+    def dd_fuel_cost_change(self, **event_args):
         """This method is called when an item is selected"""
         pass
 
-    def drop_down_8_change(self, **event_args):
+    def dd_energy_inflation_change(self, **event_args):
         """This method is called when an item is selected"""
         pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,4 +1,146 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.server
+import pandas as pd
+from datetime import datetime
+import plotly.graph_objects as go
+import plotly.express as px
+import uuid
 
 
+#Get user id for session 
+#https://anvil.works/forum/t/how-do-you-define-a-session/606
+@anvil.server.callable
+def get_uuid():
+  if "id" not in anvil.server.session:
+    anvil.server.session['id']=str(uuid.uuid4())
 
+  return anvil.server.session['id']
+
+inputs = {}
+
+@anvil.server.callable
+def get_inputs(avg_pwr, run_time, days_op_per_year, avg_solar_irr, avg_wind_speed, cost_electric, energy_inflation, cost_fuel, user_id):
+    inputs["avg_pwr"] = avg_pwr
+    inputs["run_time"] = run_time
+    inputs["days_op_per_year"] = days_op_per_year
+    inputs["avg_solar_irr"] = avg_solar_irr
+    inputs["avg_wind_speed"] = avg_wind_speed
+    inputs["cost_electric"] = cost_electric
+    inputs["energy_inflation"] = energy_inflation
+    inputs["cost_fuel"] = cost_fuel
+    inputs["user_id"] = user_id
+    print(inputs)
+
+@anvil.server.callable
+def reset_inputs():
+    inputs.clear()
+
+@anvil.server.callable
+def run_calcs(inputs):
+    
+    avg_power_selected = float(inputs["avg_pwr"])
+    run_time_selected = float(inputs["run_time"])
+    days_year_selected = float(inputs["days_op_per_year"])
+    solar_irrad_selected = float(inputs["avg_solar_irr"])
+    wind_speed_selected = float(inputs["avg_wind_speed"])
+    fuel_cost_selected = float(inputs["cost_electric"])
+    elect_grid_cost_selected = float(inputs["energy_inflation"])
+    energy_inflation_selected = float(inputs["cost_fuel"])
+    
+    
+    # print(inputs)
+    
+    # ----------------------------------------------------------------------------------------------------------------------------------
+    # Tabular constants definition
+    
+    total_power_comsumption = avg_power_selected * run_time_selected * days_year_selected
+    
+    print(f"Total power calculated {total_power_comsumption} \n")
+    
+    capital_costs = {}
+    generators = ["piston", "MGT", "HMGT", "solar", "wind"]
+    cost_factor = {
+        "piston": [0.15],
+        "MGT": [0.0045],
+        "HMGT": [0.0045],
+        "solar": [3000],
+        "wind": [0.0082]
+    }
+    
+    generator_costs = {
+        "generator": ["piston", "MGT", "HMGT", "solar", "wind"],
+        "pounds_per_kwh": [300, 500, 600, 2000, 4500]
+    }
+    
+    generator_efficiency = {
+        "generator_size_kw": ["20.0", "30.0", "40.0", "60.0", "75.0", "100.0", "125.0", "135.0", "150.0", "175.0", "200.0"],
+        "45%_l/h": [7.3, 13.2, 18.2, 21.8, 27.7, 33.6, 41.4, 44.6, 49.6, 57.7, 65.5],
+        "35%_l/h": [8, 14.5, 20, 24, 30.5, 37, 45.5, 49, 54.5, 63.5, 72],
+        "50%_l/h": [5.46, 11.87, 16.37, 19.64, 24.96, 30.28, 37.23, 40.1, 44.6, 51.96, 58.92],
+    }
+    
+    generator_efficiency_df = pd.DataFrame.from_dict(generator_efficiency).set_index(
+        "generator_size_kw"
+    )
+    generator_cost_df = pd.DataFrame.from_dict(generator_costs).set_index("generator")
+    cost_factor_df = pd.DataFrame.from_dict(cost_factor)
+    
+    # ----------------------------------------------------------------------------------------------------------------------------------
+    # Calculations
+    
+    for item in generators:
+        generator_cost_obj = generator_cost_df["pounds_per_kwh"][item]
+        capital_costs[item] = {
+            "Initial capital cost": generator_cost_obj * avg_power_selected
+        }
+    
+        if item == "solar":
+            capital_costs[item]["Yearly maintenance costs"] = cost_factor["solar"][0]
+            capital_costs[item]["Yearly fuel costs"] = 0
+        else:
+            capital_costs[item]["Yearly maintenance costs"] = (
+                total_power_comsumption * cost_factor[item][0]
+            )
+    
+        if item == "wind":
+            capital_costs[item]["Yearly fuel costs"] = 0
+    
+        if item not in ["solar", "wind"]:
+            if item == "piston":
+                efficiency = "45%_l/h"
+            elif item == "MGT":
+                efficiency = "35%_l/h"
+            elif item == "HMGT":
+                efficiency = "50%_l/h"
+            fuel_data = generator_efficiency_df.loc[str(avg_power_selected)][efficiency]
+            # print(f"fuel data: {fuel_data}, for system {item}")
+            capital_costs[item]["Yearly fuel costs"] = (
+                fuel_data * run_time_selected * days_year_selected * fuel_cost_selected
+            )
+    
+    capital_costs_df = pd.DataFrame.from_dict(capital_costs)
+    print(f"Capital costs \n {capital_costs_df} \n")
+    
+    # -------------------------------------------------------------------------------------------------------------------------------
+    # Calculate cumulative costs
+    
+    years = [i for i in range(0, 11)]
+    # print(self.years)
+    
+    cumulative_cost = {}
+    
+    for item in generators:
+        cumulative_cost[item] = [
+            capital_costs[item]["Initial capital cost"]
+            + i
+            * (
+                capital_costs[item]["Yearly maintenance costs"]
+                + capital_costs[item]["Yearly fuel costs"]
+            )
+            for i in years
+        ]
+    cumulative_cost_df = pd.DataFrame.from_dict(cumulative_cost)
+    
+    print(f"Cumulative costs \n {cumulative_cost_df} \n")
